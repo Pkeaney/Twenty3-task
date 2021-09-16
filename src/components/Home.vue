68c215b55e2779165118dea770e93f38f7c0783f
patrickkeaney14 <patrickkeaney14@gmail.com>
2021-09-16 11:14:42 +0100

    My first commit, Creating the Axios GET request

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,31 @@
 <template>
-  <div></div>
+<div>
+</div>
 </template>
 
 <script>
+//importing axios and vue axios
+import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
+Vue.use(VueAxios,axios)
+
 export default {
   name: "Home",
-};
+  data()  {
+    return {list:undefined}
+  },
+  mounted() 
+  {
+    //get request to the http
+    Vue.axios.get('https://jsonplaceholder.typicode.com/users')
+    .then((resp)=>{
+      this.list = resp
+      console.warn(resp)
+    })
+}
+}
 </script>
 
 <style scoped></style>
